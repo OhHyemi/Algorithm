fa763882b20a162d44cf6af308f3e0ba780641ab
OhHyemi <gpal1014@naver.com>
2021-11-12 23:36:29 +0900

    11/12 알고리즘

---
+++ Algorithm/Algorithm7/Beakjun1929.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithm7
+{
+    class Beakjun1929
+    {
+        static void Main(string[] args)
+        {
+            var list = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+            int m = list[0];
+            int n = list[1];
+
+            List<int> result = new List<int>();
+            //소수의 정의 : 1과 자기자신으로 밖에 나눌 없는 것..
+            //소수는 다 홀수이다. => 2x+ 1 (x >= 0)
+            // y(2x + 1) => 가 아닌 나머지 홀수 (y > 1)
+            
+            for (int i = m; i <= n; i++)
+            {
+                if (i == 1)
+                {
+                    continue;
+                }
+                if (i is > 1 and < 4)
+                {
+                    result.Add(i);
+                    continue;
+                }
+                if (i % 2 == 0)
+                {
+                    continue;
+                }
+                bool primeNum = true;
+                for (int k = 1; (k * 2 + 1) <= Math.Sqrt(i); k++)
+                {
+                    if (i % (k * 2 + 1) == 0)
+                    {
+                        primeNum = false;
+                        break;
+                    }
+                }
+                if (primeNum == true)
+                {
+                    result.Add(i);
+                }
+              
+            }
+            StringBuilder sb = new StringBuilder(String.Join("\n", result.ToArray()));
+  
+            Console.WriteLine(sb);
+        }
+    }
+}
+
---
+++ Algorithm/Algorithm8/Beakjun1181.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithm8
+{
+    class Beakjun1181
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = new List<string>();
+            var count = int.Parse(Console.ReadLine()!);
+            
+            for (int i = 0; i < count; i++)
+            {
+                string word = Console.ReadLine();
+                if (words.Exists(x=> x== word) == false)
+                {
+                    words.Add(word);
+                }
+            }
+            //알파벳 순으로 정렬
+            var list = words.OrderBy(x => x);
+            var result = list.OrderBy(x => x.Length);
+            
+            Console.WriteLine(string.Join("\n", result));
+        }
+    }
+}
+
+//QuickSort.....열쉬미..만들었는데...시..무..룩
+// List<string> words = new List<string>(); 
+//            var count = int.Parse(Console.ReadLine()!);
+//     
+//            for (int i = 0; i < count; i++)
+//            {
+//                string word = Console.ReadLine();
+//                if (words.Exists(x=> x== word) == false)
+//                {
+//                    words.Add(word);
+//                }
+//            }
+//            
+//            words.Sort();
+//            int left = 0;
+//            int right = words.Count - 1;
+//            
+//            Sort(left, right);
+//
+//            void Sort(int _left, int _right)
+//            {
+//                if (_left >= _right)
+//                {
+//                    return;
+//                }
+//                var pivot = Swap(_left, _right);
+//                Sort(_left, pivot - 1);
+//                Sort(pivot + 1, _right);
+//            }
+//            int Swap(int _left, int _right)
+//            {
+//                int _pivot = _left;
+//                _left++;
+//                while (true)
+//                {
+//                    if (words[_pivot].Length >= words[_left].Length)
+//                    {
+//                        if (words[_pivot].Length == words[_left].Length)
+//                        {
+//                            for (int i = 0; i < words[_pivot].Length; i++)
+//                            {
+//                                if (words[_pivot][i] < words[_left][i])
+//                                {
+//                                    break;
+//                                }
+//                                else if(words[_pivot][i] > words[_left][i])
+//                                {
+//                                    _left++;
+//                                    break;
+//                                }
+//                            }
+//                        }
+//                        else
+//                        {
+//                            _left++;
+//                        }
+//                    }
+//                    if (words[_pivot].Length <= words[_right].Length)
+//                    {
+//                        if (words[_pivot].Length == words[_right].Length)
+//                        {
+//                            for (int i = 0; i < words[_pivot].Length; i++)
+//                            {
+//                                if (words[_pivot][i] > words[_left][i])
+//                                {
+//                                    break;
+//                                }
+//                                else if (words[_pivot][i] < words[_right][i])
+//                                {
+//                                    _right--;
+//                                    break;
+//                                }
+//                            }
+//                        }
+//                        else
+//                        {
+//                            _right--;
+//                        }
+//                    }
+//
+//                    if (_left > _right)
+//                    {
+//                        break;
+//                    }
+//                    if (words[_left].Length > words[_right].Length)
+//                    {
+//                        (words[_left], words[_right]) = (words[_right], words[_left]);
+//                    }
+//                }
+//                (words[_pivot], words[_right]) = (words[_right], words[_pivot]);
+//                return _right;
+//            }
+//            Console.WriteLine(string.Join("\n", words));
