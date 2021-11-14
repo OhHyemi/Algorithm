b63721b2deaa60880a64e6bfb5b8fdb3c0811f99
OhHyemi <gpal1014@naver.com>
2021-11-15 00:51:40 +0900

    algorithm9 push & editing naming

--- Algorithm/Algorithm1/Baekjoon11399.cs
+++ Algorithm/Algorithm1/Baekjoon11399.cs
@@ -3,7 +3,7 @@ using System.Linq;
 
 namespace Algorithm
 {
-    class Baekjun11399
+    class Baekjoon11399
     {
         public static void Main()
         {
---
+++ Algorithm/Algorithm9/Baekjoon9461.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace Algorithm9
+{
+    class Baekjoon9461
+    {
+        static void Main(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+            var n = new int[t];
+            for (int i = 0; i < t; i++)
+            {
+                n[i] = int.Parse(Console.ReadLine()!);
+            }
+            
+            var list = n.ToList();
+            list.Sort();
+            
+            var max = list[t - 1];
+
+            var padovan = new long[max];
+
+            padovan[0] = 1;
+            padovan[1] = 1;
+            padovan[2] = 1;
+
+            while (padovan[^1] == 0)
+            {
+                for (int i = 3; i < max; i++)
+                {
+                    if (padovan[i] != 0)
+                    {
+                        continue;
+                    }
+                    padovan[i] = padovan[i - 3] + padovan[i - 2];
+                }
+            }
+
+            var result = new long[t];
+
+            for (int i = 0; i < t; i++)
+            {
+                result[i] = padovan[n[i] - 1];
+            }
+            
+            Console.WriteLine(String.Join("\n", result));
+
+        }
+    }
+}
