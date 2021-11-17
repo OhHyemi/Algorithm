cd49c77be13d8e5bf86309534007662066d663f6
OhHyemi <gpal1014@naver.com>
2021-11-17 23:17:26 +0900

    algorithm 10/11

---
+++ Algorithm/Algorithm10/Baekjoon1053.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithm10
+{
+    class Baekjon1541
+    {
+        static void Main(string[] args)
+        {
+            var op = new char[] {'+', '-'};
+            var input = Console.ReadLine();
+            var opList =  input!.ToCharArray().Where(a => a is '+' or '-').ToArray();
+            var nList = input!.Split(op).Select(int.Parse).ToArray();//연산자를 기준으로 SPLIT
+
+            if (nList.Length == 1) //수 하나만 들어왔을 경우(연산자가 없는 경우)
+            {
+                Console.WriteLine(nList[0]);
+                return;
+            }
+            int re = nList[0];
+
+            List<int> results = new List<int>();//결과값들을 저장해 놓을 공간(-기준으로 끊어 +연산한 값들)
+
+            for (int i = 0; i < opList.Length; i++)
+            {
+                if (opList[i] == '+')
+                {
+                   re += nList[i + 1];
+                }
+                else
+                {
+                    results.Add(re);//-를 만나기 전까지 더한 수의 합을 ADD
+                    re = nList[i + 1];// -를 만난 후 다음 수
+                }
+
+                if (i == opList.Length - 1)//다음 연산자가 없을 경우
+                {
+                    results.Add(re);
+                }
+            }
+
+            var result = results[0];
+
+            for (int i = 1; i < results.Count; i++)
+            {
+                result -= results[i];//처음 값에서 나머지 값을 모두 빼줌
+            }
+            
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Algorithm/Algorithm11/Baekjoon13116.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithm11
+{
+    class Baekjoon13116
+    {
+        static void Main(string[] args)
+        {
+            int tCount = int.Parse(Console.ReadLine()!);
+            var tlist = new List<int>();
+            for (int i = 0; i < tCount; i++)
+            {
+                var input = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+                tlist.AddRange(input);
+            }
+
+            for (int i = 0; i < tCount; i++)
+            {
+                int n1 = tlist[2 * i];
+                int n2 = tlist[2 * i + 1];
+            
+                while (n1 != n2)
+                {
+                    while (n1 < n2)
+                    {
+                        n2 /= 2;
+                    }
+                    if (n1 == n2)
+                    {
+                        break;
+                    }
+                    n1 /= 2;
+                }
+                Console.WriteLine(n1 * 10);
+            }
+           
+        }
+    }
+}
