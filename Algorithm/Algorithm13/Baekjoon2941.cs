e1279deab2c5f52c7dd92cf0880bedf4ea5c4c90
OhHyemi <gpal1014@naver.com>
2021-11-23 22:21:13 +0900

    algorithm13 - 백준2941

---
+++ Algorithm/Algorithm13/Baekjoon2941.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm13
+{
+    class Baekjoon2941
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string> {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+
+            var input = new StringBuilder(Console.ReadLine());
+
+            foreach (var s in list)
+            {
+                input.Replace(s, "*");//해당되는 문자열을 다른 문자로 변경
+            }
+            Console.WriteLine(input.Length);
+        }
+        
+    }
+}
