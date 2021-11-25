1138bb08a1b11fa7eb77cd075631f72d0dfc4cc7
OhHyemi <gpal1014@naver.com>
2021-11-26 00:39:17 +0900

    algorithm14 - 백준 11068

---
+++ Algorithm/Algorithm14/Baekjoon11068.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithm14
+{
+    class Baekjoon11068
+    {
+        static void Main(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+            var arr = new int[t];
+            for (int i = 0; i < t; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine()!);
+            }
+            
+            for (int i = 0; i < t; i++)
+            {
+                int result = 0;
+                    for (int j = 2; j <= 64; j++)
+                    {
+                        var convert = ConvertJ(arr[i], j);
+                        result = Check(convert);
+                        if (result == 1)
+                        {
+                            break;
+                        }
+                    }
+                
+                Console.WriteLine(result);
+            }
+
+            int Check(List<int> list)
+            {
+                for (int i = 0; i < list.Count / 2; i++)
+                {
+                    if (list[i] != list[list.Count - i - 1])
+                    {
+                        return 0;
+                    }
+                }
+                return 1;
+            }
+
+            List<int> ConvertJ(int value, int j)
+            {
+                List<int> list = new List<int>();
+                
+                while (value / j > 0)
+                {
+                    list.Add(value % j);
+                    value /= j;
+                }
+                list.Add(value);
+
+                return list;
+            }
+        }
+    }
+}
