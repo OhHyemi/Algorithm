629496a18e79b79807cf150dd57ead3ff870df9e
OhHyemi <gpal1014@naver.com>
2021-11-29 21:34:54 +0900

    algoritm16,17 1260,11724

---
+++ Algorithm/Algorithm16/DFS.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithm16
+{
+    class Baekjoon1260
+    {
+        private static List<int>[] ve;//정점과 정점마다 연결된 다른 정점들의 리스트의 배열
+        private static List<int> visited;//방문한 정점
+
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+            ve = new List<int>[input[0] + 1]; //정점의 갯수 만큼 생성.
+            visited = new List<int>();
+            for (int i = 1; i < input[0] + 1; i++)
+            {
+                ve[i] = new List<int>();
+            }
+            
+            for (int i = 1; i <= input[1]; i++)
+            {
+                var input2 = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+                ve[input2[0]].Add(input2[1]);
+                ve[input2[1]].Add(input2[0]);
+            }
+
+            for (int i = 1; i < ve.Length; i++)
+            {
+                ve[i].Sort();
+            }
+
+            DFS(input[2]);
+            Console.WriteLine(string.Join(' ', visited));
+            visited.Clear();
+            
+            BFS(input[2]);
+            Console.WriteLine(string.Join(' ', visited));
+        }
+
+        static void DFS(int v)//dfs
+        {
+            if (visited.Contains(v)) //이미 방문한  곳이라면 return
+            {
+                return;
+            }
+
+            visited.Add(v);//방문한 정점추가
+            for (int i = 0; i < ve[v].Count; i++)
+            {
+                if (!visited.Contains(ve[v][i]))//방문하지 않은 인접 정점을 방문하러가쟈!
+                {
+                    DFS(ve[v][i]);
+                }
+            }
+        }
+        static void BFS(int v)//bfs
+        {
+            Queue<int> queue_bfs = new Queue<int>();
+            queue_bfs.Enqueue(v);
+            visited.Add(v);//방문해따!
+            while (queue_bfs.Count > 0)
+            {
+                var w = queue_bfs.Dequeue();//방문한 정점을 빼면서
+            
+                for (int i = 0; i < ve[w].Count; i++)//인접한 정점들을 탐색
+                {
+                    if (!visited.Contains(ve[w][i]))//방문하지 않은 정점이 있다면
+                    {
+                        queue_bfs.Enqueue(ve[w][i]);
+                        visited.Add(ve[w][i]);//방문!
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Algorithm/Algorithm17/Baekjoon11724.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithm17
+{
+    class Baekjoon11724
+    {
+        private static List<int>[] ve;
+        static List<int> visited;
+
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+            ve = new List<int>[input[0] + 1];
+
+            for (int i = 1; i < input[0] + 1; i++)
+            {
+                ve[i] = new List<int>();
+            }
+
+            for (int i = 1; i <= input[1]; i++)
+            {
+                var input2 = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+                ve[input2[0]].Add(input2[1]);
+                ve[input2[1]].Add(input2[0]);
+            }
+
+            visited = new List<int>();
+            int count = 0; //연결된 요소 카운트
+
+            for (int i = 1; i < ve.Length; i++)
+            {
+                var list = new List<int>();
+                if (!visited.Contains(i))//방문한 곳이 아니라면
+                {
+                    DFS(i,list);
+                    if (list.Count > 0)//연결된 정점들이 하나 이상이면
+                    {
+                        visited.AddRange(list); //방문한 정점 추가
+                        count++;//연결 요소 카운트 ++
+                    }
+                }
+            }
+            Console.WriteLine(count);
+        }
+        static void DFS(int v, List<int> result) //dfs
+        {
+            if (result.Contains(v)) //이미 방문한  곳이라면 return
+            {
+                return;
+            }
+            result.Add(v); //방문한 정점추가
+            for (int i = 0; i < ve[v].Count; i++)
+            {
+                if (!result.Contains(ve[v][i])) //방문하지 않은 인접 정점을 방문하러가쟈!
+                {
+                    DFS(ve[v][i], result);
+                }
+            }
+        }
+    }
+}
